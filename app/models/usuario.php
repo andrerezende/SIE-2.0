52f134c732be31fe8e111a660278be9885549d49
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2011-11-23 09:59:43 -0200

    Validação do campo senha do model Usuário.

--- app/models/usuario.php
+++ app/models/usuario.php
@@ -15,6 +15,16 @@ class Usuario extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
+		'senha' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
 	);
 
 	public $belongsTo = array(
