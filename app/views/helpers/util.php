eedb665c169025a0323c5dec42e1c956d27d0e20
Vitor Pacheco <vitor.pacheco@ifbaiano.edu.br>
2012-01-24 12:20:47 -0200

    Adiciando método 'imgFromBoolean' no UtilHelper.

--- app/views/helpers/util.php
+++ app/views/helpers/util.php
@@ -22,10 +22,20 @@ class UtilHelper extends AppHelper {
 	 * @return string
 	 */
 	public function boolean($field) {
-		if ($field == 0) {
+		if ($field == 0 || $field == false) {
 			return 'Não';
 		} else {
 			return 'Sim';
 		}
 	}
+
+	public function imgFromBoolean($field, array $options) {
+		$options += array('class' => 'homologacao', 'val' => (int)$field);
+		if ($field == 0 || $field == false) {
+			return $this->Html->image('icons' . DS . 'onebit_33.png', $options);
+		} else {
+			return $this->Html->image('icons' . DS . 'onebit_34.png', $options);
+		}
+	}
+
 }
